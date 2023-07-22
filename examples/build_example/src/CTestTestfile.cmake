7b4ced673e3e42fddb183fdb035f054e7d354bbe
Stepan Ulyanov <stepan.ulyanov.2007@gmail.com>
2023-07-22 18:18:42 +0400

    rename examples

---
+++ examples/build_example/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/forWork/projects/C++ - Standart/SearchEngine/Search_Engine/src
+# Build directory: D:/forWork/projects/C++ - Standart/SearchEngine/Search_Engine/examples/build/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
